1791d277de80936b4636f0b65bc7bd6f5dcd8651
PhillipHenry <PhillHenry@gmail.com>
2023-06-29 11:00:09 +0100

    Received request api key WRITE_TXN_MARKERS with version 1 which is not enabled

---
+++ modules/core/src/main/scala/uk/co/odinconsultants/kafka/KafkaDemoMain.scala
@@ -0,0 +1,52 @@
+package uk.co.odinconsultants.kafka
+
+import cats.effect.{Deferred, IO, IOApp}
+import cats.free.Free
+import com.comcast.ip4s.*
+import fs2.Stream
+import uk.co.odinconsultants.dreadnought.Flow.race
+import uk.co.odinconsultants.dreadnought.docker.*
+import uk.co.odinconsultants.dreadnought.docker.Algebra.toInterpret
+import uk.co.odinconsultants.dreadnought.docker.CatsDocker.interpret
+import uk.co.odinconsultants.dreadnought.docker.KafkaAntics.produceMessages
+import uk.co.odinconsultants.dreadnought.docker.Logging.verboseWaitFor
+import uk.co.odinconsultants.dreadnought.docker.PopularContainers.startKafkaOnPort
+import uk.co.odinconsultants.dreadnought.docker.SparkStructuredStreamingMain.{startSlave, startSparkCluster, waitForMaster}
+import uk.co.odinconsultants.dreadnought.docker.ZKKafkaMain.{kafkaEcosystem, startKafkaCluster}
+
+import scala.concurrent.duration.*
+
+object KafkaDemoMain extends IOApp.Simple {
+
+  val TOPIC_NAME = "test_topic"
+  val BOOTSTRAP  = "kafka_bootstrap"
+  val BROKER     = "zk"
+  val kafkaPort  = port"9093"
+
+  /** TODO
+    * Pull images
+    */
+  def run: IO[Unit] = for {
+    client       <- CatsDocker.client
+    (zk, kafka1) <- startKafkaCluster(client, verboseWaitFor, 20.seconds)
+    zkName       <- CatsDocker.interpret(client, Free.liftF(NamesRequest(zk)))
+    kafkaStart   <- Deferred[IO, String]
+    kafkaLatch    = verboseWaitFor("started (kafka.server.Kafka", kafkaStart)
+    kafka2       <- interpret(client, Free.liftF(startKafkaOnPort(kafkaPort, zkName)))
+    _            <- interpret(client, Free.liftF(
+                      LoggingRequest(kafka2, kafkaLatch)
+                    ))
+    _            <- kafkaStart.get.timeout(20.seconds)
+    _            <- sendMessages
+    _            <- race(toInterpret(client))(
+                      List(kafka2, kafka1, zk).map(StopRequest.apply)
+                    )
+  } yield println("Started and stopped ZK and 2 kafka brokers")
+
+  private val sendMessages =
+    produceMessages(ip"127.0.0.1", kafkaPort, TOPIC_NAME)
+      .handleErrorWith(x => Stream.eval(IO(x.printStackTrace())))
+      .compile
+      .drain
+
+}
