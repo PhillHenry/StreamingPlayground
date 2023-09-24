f09aebbbb15fe990aef7e6b3fcdead577920e689
PhillipHenry <PhillHenry@gmail.com>
2023-09-24 16:55:47 +0100

    This works.

--- modules/core/src/main/scala/uk/co/odinconsultants/sss/SparkS3Main.scala
+++ modules/core/src/main/scala/uk/co/odinconsultants/sss/SparkS3Main.scala
@@ -1,6 +1,7 @@
 package uk.co.odinconsultants.sss
-import org.apache.spark.sql.SaveMode
-import uk.co.odinconsultants.SparkUtils.sparkS3Session
+import org.apache.spark.sql.{SaveMode, SparkSession}
+import uk.co.odinconsultants.S3Utils.{BUCKET_NAME, MINIO_ROOT_PASSWORD, MINIO_ROOT_USER, URL_S3, load_config}
+import uk.co.odinconsultants.SparkUtils.{SPARK_DRIVER_PORT, sparkS3Session}
 
 /**
  * See https://medium.com/@dineshvarma.guduru/reading-and-writing-data-from-to-minio-using-spark-8371aefa96d2
@@ -8,9 +9,19 @@ import uk.co.odinconsultants.SparkUtils.sparkS3Session
 object SparkS3Main {
 
   def main(args: Array[String]): Unit = {
-    val session = sparkS3Session(args(0))
+    SparkStructuredStreamingMain.createLocalDnsMapping()
+    val session = load_config(
+      SparkSession.builder
+        .appName("local")
+        .master("local")
+        .getOrCreate(),
+      MINIO_ROOT_USER,
+      MINIO_ROOT_PASSWORD,
+      URL_S3,
+    )
     println("About to write....")
-    session.range(1000).write.mode(SaveMode.Overwrite).parquet("s3a://test2/test")
+    val path  = s"s3a://$BUCKET_NAME/test_parquet"
+    session.range(1000).write.mode(SaveMode.Overwrite).parquet(path)
   }
 
 }
