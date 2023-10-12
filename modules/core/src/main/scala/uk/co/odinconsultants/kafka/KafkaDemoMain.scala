2e8c090bf28022ddacafd70bf9ce37bcc038b923
PhillipHenry <PhillHenry@gmail.com>
2023-10-12 16:33:52 +0100

    Kafka now uses Raft.

--- modules/core/src/main/scala/uk/co/odinconsultants/kafka/KafkaDemoMain.scala
+++ modules/core/src/main/scala/uk/co/odinconsultants/kafka/KafkaDemoMain.scala
@@ -4,60 +4,85 @@ import cats.effect.{Deferred, IO, IOApp}
 import cats.free.Free
 import com.comcast.ip4s.{port, *}
 import fs2.Stream
-import fs2.kafka.ProducerSettings
+import fs2.kafka.{AutoOffsetReset, CommittableConsumerRecord, ConsumerRecord, ConsumerSettings, KafkaConsumer, ProducerSettings}
 import uk.co.odinconsultants.dreadnought.Flow.race
 import uk.co.odinconsultants.dreadnought.docker.*
 import uk.co.odinconsultants.dreadnought.docker.Algebra.toInterpret
 import uk.co.odinconsultants.dreadnought.docker.CatsDocker.interpret
 import uk.co.odinconsultants.dreadnought.docker.KafkaAntics.{createCustomTopic, produceMessages}
-import uk.co.odinconsultants.dreadnought.docker.Logging.verboseWaitFor
+import uk.co.odinconsultants.dreadnought.docker.Logging.{ioPrintln, verboseWaitFor}
 import uk.co.odinconsultants.dreadnought.docker.PopularContainers.startKafkaOnPort
 import uk.co.odinconsultants.dreadnought.docker.SparkStructuredStreamingMain.{startSlave, startSparkCluster, waitForMaster}
 import uk.co.odinconsultants.dreadnought.docker.ZKKafkaMain.{kafkaEcosystem, startKafkaCluster}
 import uk.co.odinconsultants.sss.SSSUtils.TOPIC_NAME
+import uk.co.odinconsultants.sss.SparkStructuredStreamingMain.{OUTSIDE_KAFKA_BOOTSTRAP_PORT, ioLog, networkName}
 
 import java.util.UUID
 import scala.concurrent.duration.*
 
 object KafkaDemoMain extends IOApp.Simple {
 
-  val BROKER     = "zk"
-  val kafkaPort  = port"9093"
+  val BROKER          = "zk"
+  val kafkaPort       = port"9111"
+  val bootstrapServer = s"localhost:${kafkaPort.value}"
 
   /** TODO
     * Pull images
     */
   def run: IO[Unit] = for {
-    client       <- CatsDocker.client
-    (zk, kafka1) <- startKafkaCluster(client, verboseWaitFor(Some(s"${Console.RED}SparkMaster: ")), 20.seconds)
-    zkName       <- CatsDocker.interpret(client, Free.liftF(NamesRequest(zk)))
-    kafkaStart   <- Deferred[IO, String]
-    kafkaLatch    = verboseWaitFor(Some(s"${Console.BLUE}Kafka: "))("started (kafka.server.Kafka", kafkaStart)
-    kafka2       <- interpret(client, Free.liftF(startKafkaOnPort(kafkaPort, zkName)))
-    _            <- interpret(
-                      client,
-                      Free.liftF(
-                        LoggingRequest(kafka2, kafkaLatch)
-                      ),
-                    )
-    _            <- kafkaStart.get.timeout(20.seconds)
-    _            <- sendMessages
-    _            <- race(toInterpret(client))(
-                      List(kafka2, kafka1, zk).map(StopRequest.apply)
-                    )
+    client     <- CatsDocker.client
+    kafkaStart <- Deferred[IO, String]
+    kafkaLatch  =
+      verboseWaitFor(Some(s"${Console.BLUE}kafka1: "))("started (kafka.server.Kafka", kafkaStart)
+    loggers     = List(
+                    kafkaLatch,
+                    ioPrintln(Some(s"${Console.GREEN}kafka2: ")),
+                    ioPrintln(Some(s"${Console.YELLOW}kafka3: ")),
+                  )
+    kafkas     <-
+      interpret(
+        client,
+        KafkaRaft.startKafkas(loggers, networkName),
+      )
+    _          <- kafkaStart.get.timeout(20.seconds)
+    _          <- ioLog(s"About to create topic $TOPIC_NAME")
+    _          <- IO(createCustomTopic(TOPIC_NAME, OUTSIDE_KAFKA_BOOTSTRAP_PORT))
+    _          <- sendMessages
+    _          <- consume(
+                    ConsumerSettings[IO, String, String]
+                      .withAutoOffsetReset(AutoOffsetReset.Earliest)
+                      .withBootstrapServers(bootstrapServer)
+                      .withGroupId("group_PH"),
+                    TOPIC_NAME,
+                  ).compile.drain.start
+    _          <- ioLog("About to close down. Press return to end") *> IO.readLine
+    _          <- race(toInterpret(client))(
+                    kafkas.map(StopRequest.apply)
+                  )
   } yield println("Started and stopped ZK and 2 kafka brokers")
 
   private val sendMessages: IO[Unit] = {
-    val bootstrapServer                                        = s"localhost:${kafkaPort.value}"
     val producerSettings: ProducerSettings[IO, String, String] =
       ProducerSettings[IO, String, String]
         .withBootstrapServers(bootstrapServer)
-    val messages                                               = KafkaAntics
+    val messages: IO[Unit]                                     = KafkaAntics
       .produce(producerSettings, TOPIC_NAME)
-      .handleErrorWith(x => Stream.eval(IO(x.printStackTrace())))
+      .handleErrorWith((x: Throwable) => Stream.eval(IO(x.printStackTrace())))
       .compile
       .drain
-    IO(createCustomTopic(TOPIC_NAME, port"9091")) *> messages
+    IO(createCustomTopic(TOPIC_NAME, kafkaPort)) *> messages
   }
 
+  def consume(
+      consumerSettings: ConsumerSettings[IO, String, String],
+      topic:            String,
+  ): Stream[IO, CommittableConsumerRecord[IO, String, String]] =
+    KafkaConsumer
+      .stream(consumerSettings)
+      .subscribeTo(topic)
+      .records
+      .evalMap { (committable: CommittableConsumerRecord[IO, String, String]) =>
+        val record: ConsumerRecord[String, String] = committable.record
+        ioLog(s"Consumed ${record.key} -> ${record.value}") *> IO(committable)
+      }
 }
